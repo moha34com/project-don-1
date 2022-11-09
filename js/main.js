a6be1b81bc5786780afb38f2bbc7db5d9ed3de77
moha34com@gmail.com <moha34com@gmail.com>
2022-11-09 10:59:31 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// // model
+// const model = (function(){
+
+
+
+// })()
+// // view
+// const view = (function(){
+//     const ob_el = {
+//         row1     : document.querySelector('#row1'),
+//         row2     : document.querySelector('#row2'),
+//         btn_load : document.querySelector('#load'),
+//     }
+//     const row1 = function(){
+//         return ob_el.row1;
+//     }
+//     const row2 = function(){
+//         return ob_el.row2;
+//     }
+//     const btn_load = function(){
+//         return ob_el.btn_load;
+//     }
+
+// return{
+//     row1,
+//     row2,
+//     btn_load,
+// }
+
+
+
+
+// })()
+// // controller
+// const controller = (function(){
+
+//     const appear_btn =  function(){  
+//         view.btn_load().onclick = function(){
+//             console.log('my name')
+//             view.row2().classList.toggle('vanish');
+            
+//     }
+// }
+//     return{
+//         appear_btn,
+//     }
+// })()
+// controller.appear_btn();
